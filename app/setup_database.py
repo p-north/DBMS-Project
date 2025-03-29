62cf66abf8dbe3719a9e7801ab2f96475d222645
p-north <puneetnorth170@gmail.com>
2025-03-29 16:33:36 -0700

    add db connection function

--- app/setup_database.py
+++ app/setup_database.py
@@ -1,2 +1,12 @@
-# DB Connection 
+
 # DB Schema
+import sqlite3
+
+def connect_db():
+    # Create and return the database connection
+    conn = conn = sqlite3.connect('library.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+
