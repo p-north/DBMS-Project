ac132286340ceb0097823c9c009ab2e8c7ba32af
p-north <puneetnorth170@gmail.com>
2025-03-30 21:56:41 -0700

    add login functionality

---
+++ app/services/memberLogin.py
@@ -0,0 +1,31 @@
+import sqlite3
+from ..setup_database import connect_db
+
+# Function to allow the user to sign in as library member
+def login(email):
+     # connect to the db
+    conn = connect_db()
+    
+    # create cursor
+    cursor = conn.cursor()
+    
+    try:
+        # check if the user exists 
+        userQuery = "SELECT * FROM member m WHERE m.email = ?;"
+        cursor.execute(userQuery, (email,))
+        member = cursor.fetchone()
+        # return if member not found
+        if not member:
+            print("Member not found. Please provide a different email.")
+            return None
+        firstName = member["firstName"]
+        memberID = member["memberID"]
+        print("Login success! Welcome,", firstName)
+    except sqlite3.Error as e:
+        print("Error logging in", e)
+        
+    # close the connection
+    conn.close()
+    
+# Testing purposes only-------------------------
+# login('michael.brown@email.com')
